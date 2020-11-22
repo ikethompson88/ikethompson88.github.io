0701fef936743f8257799ac45872d661ffb20347
ikethompson88 <ikethompson88@byui.edu>
2020-11-21 16:16:49 -0800

    update

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -5,13 +5,17 @@ fetch(apiURL)
     .then((jsObject) => {
         console.log(jsObject);
 
-        document.getElementById('current-temp').textContent = jsObject.main.temp;
+        const curtemp = document.querySelector('#current-temp');
+        const iconsource = document.querySelector('#imagesrc');
+        const weathericon = document.querySelector('#icon');
 
-        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; // note the concatenation
-        const desc = jsObject.weather[0].description; // note how we reference the weather array
 
-        document.getElementById('imagesrc').textContent = imagesrc; // informational specification only
-        document.getElementById('icon').setAttribute('src', imagesrc); // focus on the setAttribute() method
-        document.getElementById('icon').setAttribute('alt', desc);
+
+        curtemp.innerHTML = jsObject.main.temp;
+        iconsource.textContent = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+        const isrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+
+        weathericon.setAttribute('src', isrc)
+        weathericon.setAttribute('alt', jsObject.weather[0].description);
 
     });
