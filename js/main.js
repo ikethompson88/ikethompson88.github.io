568eef2bdda722a809a5018d284f188316715a7c
ikethompson88 <ikethompson88@byui.edu>
2020-12-17 08:09:10 -0800

    term project update

--- js/main.js
+++ js/main.js
@@ -3,3 +3,7 @@ let updated = document.lastModified;
 
 document.getElementById("year").innerHTML = date.getFullYear();
 document.getElementById("last-updated").innerHTML = updated;
+
+function toggleMenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
--- term-project/js/main.js
+++
@@ -1,9 +0,0 @@
-let date = new Date();
-let updated = document.lastModified;
-
-document.getElementById("year").innerHTML = date.getFullYear();
-document.getElementById("last-updated").innerHTML = updated;
-
-function toggleMenu() {
-    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
-}
