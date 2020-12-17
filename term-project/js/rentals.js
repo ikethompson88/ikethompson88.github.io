7380db22be32571ec79647d9c7b272492e590d03
ikethompson88 <ikethompson88@byui.edu>
2020-12-17 05:48:35 -0800

    update

--- term-project/js/rentals.js
+++ term-project/js/rentals.js
@@ -1,32 +1,37 @@
-const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+const requestURL = 'https://raw.githubusercontent.com/ikethompson88/ikethompson88.github.io/master/term-project/data/rentals.JSON';
 
 fetch(requestURL)
     .then(function (response) {
         return response.json();
     })
     .then(function (jsonObject) {
-        // console.table(jsonObject); temporary checking for valid response and data parsing
-        const prophets = jsonObject['prophets'];
-        for (let i = 0; i < prophets.length; i++) {
+        console.table(jsonObject); //temporary checking for valid response and data parsing
+        const rentals = jsonObject['rentals'];
+        for (let i = 0; i < rentals.length; i++) {
 
-            let card = document.createElement('section');
-            let h2 = document.createElement('h2');
-            let p1 = document.createElement('p');
-            let p2 = document.createElement('p')
-            let image = document.createElement('img');
+            let row = document.createElement('tr');
+            let type = document.createElement('td');
+            let max = document.createElement('td');
+            let resHalf = document.createElement('td')
+            let resFull = document.createElement('td')
+            let walkHalf = document.createElement('td')
+            let walkFull = document.createElement('td')
 
-            h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-            p1.textContent = 'Date of Birth: ' + prophets[i].birthdate;
-            p2.textContent = 'Place of Birth: ' + prophets[i].birthplace;
-            image.setAttribute('src', prophets[i].imageurl);
-            image.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + ' - ' + prophets[i].order);
+            type.textContent = rentals[i].vehicle;
+            max.textContent = rentals[i].max;
+            resHalf.textContent = rentals[i].reservation-half;
+            resFull.textContent = rentals[i].reservation-full;
+            walkHalf.textContent = rentals[i].walk-half;
+            walkFull.textContent = rentals[i].walk-half;
 
-            card.appendChild(h2);
-            card.appendChild(p1);
-            card.appendChild(p2);
-            card.appendChild(image);
+            row.appendChild(type);
+            row.appendChild(max);
+            row.appendChild(resHalf);
+            row.appendChild(resFull);
+            row.appendChild(walkHalf);
+            row.appendChild(walkFull);
 
-            document.querySelector('div.cards').appendChild(card);
+            document.querySelector('table.body').appendChild(row);
 
         }
     });
